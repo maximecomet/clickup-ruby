11fc04d61773cb0fd6228adb074b37687f49397c
maximecomet <maxime.comet@koris-intl.com>
2021-08-24 18:30:29 +0200

    d15

--- lib/click_up/connection_manager.rb
+++ lib/click_up/connection_manager.rb
@@ -10,7 +10,7 @@ module ClickUp
     end
 
     def get
-      raise path.inspect
+      raise resource_url.path.inspect
       net_http_response = https_client.request_get(resource_url.path, default_headers)
       format_response(net_http_response.body)
     end
