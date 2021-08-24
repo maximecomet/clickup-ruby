d0882c272c83550cad8b454132dfcb124a77e031
maximecomet <maxime.comet@koris-intl.com>
2021-08-24 17:47:42 +0200

    test 2

--- lib/click_up/hierarchy/time_entry.rb
+++ lib/click_up/hierarchy/time_entry.rb
@@ -8,17 +8,6 @@ module ClickUp
       def index_path(params={})
         "/team/#{params[:team_id]}/time_entries"
       end
-
-      def rejected_params
-        [
-          :id,
-          :team_id
-        ]
-      end
-
-      def formatted_params(params)
-        params.reject {|key, _| rejected_params.include?(key) }
-      end
     end
   end
 end
